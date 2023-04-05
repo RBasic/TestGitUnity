7b361851589d457a7a50cbb4e49b3d8f0dee94a9
RBasic <rcham.pro@gmail.com>
2023-04-05 15:22:43 +0200

    Update TestGit.cs

--- Assets/Scripts/TestGit.cs
+++ Assets/Scripts/TestGit.cs
@@ -7,7 +7,7 @@ public class TestGit : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("COUCOU");
+        Debug.Log("Test2");
     }
 
     // Update is called once per frame
